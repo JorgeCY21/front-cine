60a4c2bdbec7dc643bafc5aa53087fb2f7d4c8cb
Carlo Valdivia Luna <cvaldivialu@unsa.edu.pe>
2025-06-18 16:24:44 -0500

    Add server proxy configuration for API requests

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://10.7.125.77:3000',
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/api/, ''),
+      }
+    }
+  }
 })

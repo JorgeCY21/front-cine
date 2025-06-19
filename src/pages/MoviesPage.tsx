b9baedb1c7657bd54adab0b7182ff121ae6a8ce1
Jorge <jcondorios@unsa.edu.pe>
2025-06-18 22:00:40 -0500

    arreglando moviepage

--- src/pages/MoviesPage.tsx
+++ src/pages/MoviesPage.tsx
@@ -179,7 +179,7 @@ export default function MoviesPage() {
           </div>
 
           {/* Movies Grid */}
-          <div className="grid grid-cols-2 xs:grid-cols-3 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4 sm:gap-6">
+          <div className="grid grid-cols-2 xs:grid-cols-3 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4 sm:gap-6 mb-10">
             {movies.map((movie) => (
               <MovieCard
                 key={movie.id}
